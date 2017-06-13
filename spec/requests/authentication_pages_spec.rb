bb41cad4ecdf662246808b55daa9889d2be8ac9e
Meredith <manson.mere@gmail.com>
2017-06-13 10:42:25 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,7 +20,7 @@ describe "Authentication" do
     it { should have_title('Sign in') }
     it { should have_selector('div.alert.alert-error') }
    
-    describe "after visitng another page" do
+    describe "after visiting another page" do
      before { click_link "Home" }
      it { should_not have_selector('div.alert.alert-error') }
     end
@@ -107,6 +107,28 @@ describe "Authentication" do
     describe "visiting the user index" do
      before { visit users_path }
      it { should have_title('Sign in') }
+    end
+
+    describe "visitng the following page" do
+     before { visit following_user_path(user) }
+     it { should have_title('Sign in') }
+    end
+
+    describe "visiting the followers page" do
+     before { visit followers_user_path(user) }
+     it { should have_title('Sign in') }
+    end
+   end
+
+   describe "in the Relationships controller" do
+    describe "submitting to the create action" do
+     before { post relationships_path }
+     specify { expect(response).to redirect_to(signin_path) }
+    end
+
+    describe "submitting to the destroy action" do
+     before { delete relationship_path(1) }
+     specify { expect(response).to redirect_to(signin_path) }
     end
    end
   end
